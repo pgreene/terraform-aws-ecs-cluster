69a177d353b71c58700f0f3240f1992c39b018cd
pgreene <pgreene@protonmail.com>
2025-02-24 15:12:47 -0500

    Updated cluster module since default_capacity_provider_strategy is no longer a block config

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "aws_ecs_cluster" "general" {
   name = var.name
-  dynamic "default_capacity_provider_strategy" {
-    for_each = var.default_capacity_provider_strategy == null ? [] : [var.default_capacity_provider_strategy]
-    content {
-      capacity_provider = default_capacity_provider_strategy.value.capacity_provider
-      weight = default_capacity_provider_strategy.value.weight
-      base = default_capacity_provider_strategy.value.base
-    }
-  }
+  #dynamic "default_capacity_provider_strategy" {
+  #  for_each = var.default_capacity_provider_strategy == null ? [] : [var.default_capacity_provider_strategy]
+  #  content {
+  #    capacity_provider = default_capacity_provider_strategy.value.capacity_provider
+  #    weight = default_capacity_provider_strategy.value.weight
+  #    base = default_capacity_provider_strategy.value.base
+  #  }
+  #}
   dynamic "setting" {
     for_each = var.setting == null ? [] : [var.setting]
     content {
